ed048456cab473637b8d0d01a4b509f42ba89892
huycans <huycan_lk@yahoo.com.vn>
2021-12-17 16:46:42 -0500

    Add cors option

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import yfinance as yf
 
 app = Flask(__name__)
-
+CORS(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def welcome():
