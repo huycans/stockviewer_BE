8342c5d10d96638c69df11ef853497039244591c
huycans <huycan_lk@yahoo.com.vn>
2022-04-16 11:14:46 -0400

    Return price history as ratio instead of raw price

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, json, request, jsonify, Response, send_from_directory
+from flask import Flask, json, request, jsonify, Response, send_from_directory, render_template
 from werkzeug.exceptions import HTTPException
 # from flask_cors import CORS
 import yfinance as yf
@@ -7,9 +7,15 @@ import numpy as np
 app = Flask(__name__, static_url_path='', static_folder='frontend/build')
 # CORS(app) # comment out when deploy
 
+
 @app.route("/", defaults={'path':''})
 def serve(path):
     return send_from_directory(app.static_folder,'index.html')
+
+# TODO: redirect all traffic to index.html
+# @app.route('/<path:page>')
+# def fallback(page):
+#     return render_template('index.html')
 
 @app.route('/get_info', methods=["POST"])
 def getInfo():
@@ -26,11 +32,13 @@ def getInfo():
     epoch_time_arr = close_price_history_obj.index.astype(np.int64)
     epoch_time_arr = (epoch_time_arr/1000000).astype(np.int64)
 
+    # TODO: use zip instead? might be faster
     # array to store price history in the shape of [[<time>, <price>], ...]
     close_price_history_arr = []    
     for idx, time in enumerate(epoch_time_arr):
         price = close_price_history_obj.values[idx]
         close_price_history_arr.append([time, price])
+    
     # if symbol does not exist in yfinance
     if ('symbol' not in ticker.info):
         return jsonify({
@@ -110,9 +118,12 @@ def getList():
     # {
     #   <ticker_name>: [price_history],...
     # }
-    close_price_history = {}
+    close_price_ratio_history = {}
     for ticker in (ticker_name_arr):
-        close_price_history[ticker] = close_column[ticker].tolist()
+        close_price = close_column[ticker]
+        # get ratios of prices from the first price, then multiply by 10000, and convert the result to int32 type to round them and save space
+        close_price = ((close_price/(close_price.iat[0]))*10000).astype('int32')
+        close_price_ratio_history[ticker] = close_price.tolist()
     
     # TODO: concurrency, should fetch price history and info in parallel
     # STAGE 2: fetch tickers info
@@ -126,7 +137,7 @@ def getList():
         "status": "ok",
             "data": {
                 "timestamps":  epoch_time_list,
-                "tickers_price_history": close_price_history,
+                "tickers_price_history": close_price_ratio_history,
                 "info": info,
             },
             'error': {}
